dfa32e4d3116f50e2096205efc5532f6bd37302e
mhndmkhtar74-png <mhndmkhtar74@gmail.com>
2026-03-05 02:59:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -7,7 +7,7 @@ from datetime import datetime
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mohannad_yemen_safe_2026'
+app.config['SECRET_KEY'] = 'mohannad_yemen_pro_2026'
 app.config["MONGO_URI"] = "mongodb+srv://mohannad:family123@cluster0.arkrscx.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0"
 
 UPLOAD_FOLDER = 'static/uploads'
@@ -34,21 +34,29 @@ def load_user(user_id):
 @login_required
 def index():
     users = list(mongo.db.users.find({"username": {"$ne": current_user.username}}))
-    messages = list(mongo.db.messages.find({"type": "public"}).sort("timestamp", 1))
-    return render_template('chat.html', users=users, messages=messages, chat_type="public")
+    return render_template('chat.html', users=users, chat_type="public")
 
 @app.route('/private/<recipient>')
 @login_required
 def private_chat(recipient):
     users = list(mongo.db.users.find({"username": {"$ne": current_user.username}}))
-    messages = list(mongo.db.messages.find({
+    return render_template('chat.html', users=users, chat_type="private", recipient=recipient)
+
+# --- ميزة جلب الرسائل الجديدة صامتاً ---
+@app.route('/get_messages/<chat_type>/<receiver>')
+@login_required
+def get_messages(chat_type, receiver):
+    query = {"type": "public"} if chat_type == "public" else {
         "type": "private",
-        "$or": [
-            {"sender": current_user.username, "receiver": recipient},
-            {"sender": recipient, "receiver": current_user.username}
-        ]
-    }).sort("timestamp", 1))
-    return render_template('chat.html', users=users, messages=messages, chat_type="private", recipient=recipient)
+        "$or": [{"sender": current_user.username, "receiver": receiver}, {"sender": receiver, "receiver": current_user.username}]
+    }
+    messages = list(mongo.db.messages.find(query).sort("timestamp", 1))
+    
+    for msg in messages:
+        msg['_id'] = str(msg['_id'])
+        msg['timestamp'] = msg['timestamp'].strftime('%I:%M %p') if 'timestamp' in msg else ""
+            
+    return jsonify({"messages": messages, "current_user": current_user.username})
 
 @app.route('/send', methods=['POST'])
 @login_required
@@ -70,13 +78,9 @@ def send():
 
     if content or file_url:
         mongo.db.messages.insert_one({
-            "sender": current_user.username,
-            "receiver": receiver,
-            "content": content,
-            "file_url": file_url,
-            "file_type": file_type,
-            "type": chat_type,
-            "timestamp": datetime.utcnow()
+            "sender": current_user.username, "receiver": receiver,
+            "content": content, "file_url": file_url, "file_type": file_type,
+            "type": chat_type, "timestamp": datetime.utcnow()
         })
     return redirect(url_for('private_chat', recipient=receiver) if chat_type == "private" else url_for('index'))
 
@@ -88,10 +92,6 @@ def login():
             login_user(User(user_data))
             return redirect(url_for('index'))
     return render_template('login.html')
-
-@app.route('/logout')
-def logout():
-    logout_user(); return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
